1b6697241c29edaddcd3e6e9310b7a3ae9009c31
Stefan Koev <st.koev@outlook.com>
2022-08-24 09:39:28 +0300

    implement javascript, final commit

--- app.js
+++ app.js
@@ -2,3 +2,16 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+"use strict";
+
+const modalBtn = document.querySelector(".modal-btn");
+const closeModalBtn = document.querySelector(".close-btn");
+const modalOverlay = document.querySelector(".modal-overlay");
+
+modalBtn.addEventListener("click", () => {
+  modalOverlay.classList.add("open-modal");
+});
+
+closeModalBtn.addEventListener("click", () => {
+  modalOverlay.classList.remove("open-modal");
+});
